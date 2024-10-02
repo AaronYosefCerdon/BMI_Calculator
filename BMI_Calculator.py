14b528a695fa45aea0ce0f1f5612c7dcf77cd58d
Aaron Yosef Cerdon <aaronyosefcerdon123@gmail.com>
2024-10-02 16:44:12 +0800

    Entering height and weight

---
+++ BMI_Calculator.py
@@ -0,0 +1,6 @@
+Height = float(input("Enter your height here in centimeters: "))
+Weight = float(input("Enter your weight here in kilograms:  "))
+
+Height = Height/100
+
+BMI = Weight/(Height*Height)
