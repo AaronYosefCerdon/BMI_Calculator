91c8c604f651011f0156ab510575f8f9447038db
Aaron Yosef Cerdon <aaronyosefcerdon123@gmail.com>
2024-10-03 18:30:42 +0800

    Results and 2 out of 4 interpretations

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -4,3 +4,10 @@ Weight = float(input("Enter your weight here in kilograms:  "))
 Height = Height/100
 
 BMI = Weight/(Height*Height)
+
+print("Your body mass index is:",BMI)
+if(BMI>0):
+    if(BMI<=16):
+        print("You are severely underweight.")
+    elif(BMI<=18.5):
+        print("You are underweight.")    
